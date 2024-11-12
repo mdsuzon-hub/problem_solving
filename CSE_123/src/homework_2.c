89bd9f1a01a74a69debefb8daa245d97c6ecee0c
sAm <67915151+mdsuzon-hub@users.noreply.github.com>
2024-11-12 13:02:46 +0600

    Update homework_2.c

--- CSE_123/src/homework_2.c
+++ CSE_123/src/homework_2.c
@@ -102,3 +102,335 @@ int main() {
     return 0;
 }
 
+// problem
+    #include <stdio.h>
+
+int main(){
+    int testcase;
+    long long int x, y, a, b;
+    long long int n, m, r, c;
+    int linear_move, vertical_move;
+    int ans = 0;
+
+    scanf("%d", &testcase);
+
+    for(int i = 0; i < testcase; i++){
+        scanf("%lld %lld %lld %lld", &x, &y, &a, &b);
+
+        ans = (y * (x - a)) + ((y - 1) * (x - a)) + (y - b);
+        
+        printf("My anser %lld\n", ans);
+    }
+}
+
+//queue
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+// creating a new queue element
+struct node* new_queue_element(int data){
+    struct node* new_queue_element = (struct node*)malloc(sizeof(struct node));
+    new_queue_element->data = data;
+    new_queue_element->next = NULL;
+
+    return new_queue_element;
+}
+
+void print_queue(struct node* front, int *size_queue){
+
+    printf("   _         _   Size of queue - %d\n", *size_queue);
+    printf("    |       |   \n");
+    printf("    |       |   \n");
+
+    while(front != NULL){
+        if(front->data >= 0 && front->data <= 9) {
+            printf("    |-------|   \n");
+            printf("    |   %d   |   \n", front->data);
+        } else if(front->data > 9 && front->data <= 99) {
+            printf("    |-------|   \n");
+            printf("    |  %d   |   \n", front->data);
+        } else if(front->data > 99 && front->data <= 999) {
+            printf("    |-------|   \n");
+            printf("    |  %d  |   \n", front->data);
+        } else {
+            printf("    |-------|   \n");
+            printf("    | %d |   \n", front->data);
+        }
+        front = front->next;
+    }
+
+    printf("    |-------|   \n");
+    printf("    |       |   \n");
+    printf("    |       |   \n");
+    printf("   _         _  \n");
+}
+
+struct node* free_memory(struct node* front, int n, int* size_queue){
+    struct node* temp;
+
+    for(int i = 0; i < n && front != NULL; i++){
+        temp = front;
+        front = front->next;
+        free(temp);
+        (*size_queue)--;
+    }
+
+    return front;
+}
+
+int main(){
+    int size_queue = 0;
+    struct node* front = NULL;
+    struct node* rear = NULL;
+    int input = -1;
+
+repeat:
+
+    printf("Clearing the terminal...\n");
+    system("cls");
+
+    print_queue(front, &size_queue);
+
+    printf("\n\n-----------------------------------------------------------\n\n");
+
+    printf("0. Exit\n");
+    printf("1. Enqueue 10 elements '1 to 10'\n");
+    printf("2. Enqueue 'n' elements\n");
+    printf("3. Dequeue '1' element\n");
+    printf("4. Dequeue 'n' elements\n\n==> ");
+
+    scanf("%d", &input);
+
+    if(input == 1){
+        for(int i = 1; i <= 10; i++){
+            struct node* new_element = new_queue_element(i);
+
+            if(rear == NULL) {
+                front = rear = new_element;
+            } else {
+                rear->next = new_element;
+                rear = new_element;
+            }
+
+            size_queue++;
+        }
+    }
+
+    if(input == 2){
+        int n, element;
+        printf("How many elements you want to enqueue\n ==> ");
+        scanf("%d", &n);
+
+        for(int i = 0; i < n; i++){
+            printf("  ==> ");
+            scanf("%d", &element);
+
+            struct node* new_element = new_queue_element(element);
+
+            if(rear == NULL) {
+                front = rear = new_element;
+            } else {
+                rear->next = new_element;
+                rear = new_element;
+            }
+
+            size_queue++;
+        }
+    }
+
+    if(input == 3){
+        if(size_queue > 0) {
+            front = free_memory(front, 1, &size_queue);
+            if(front == NULL) rear = NULL;
+        } else {
+            printf("Queue is empty!\n");
+        }
+    }
+
+    if(input == 4){
+        int n;
+        printf("How many elements you want to dequeue\n ==> ");
+        scanf("%d", &n);
+
+        if(n <= size_queue) {
+            front = free_memory(front, n, &size_queue);
+            if(front == NULL) rear = NULL;
+        } else {
+            printf("Not enough elements in the queue!\n");
+        }
+    }
+
+    if(input == 0){
+        // Free memory
+        free_memory(front, size_queue, &size_queue);
+
+        goto end;
+    }
+
+    goto repeat;
+
+end:
+    return 0;
+}
+
+// add stack
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+// creating a new stack eliment
+struct node* new_stack_eliment(int data){
+    struct node* new_stack_eliment = (struct node*)malloc(sizeof(struct node));
+    new_stack_eliment -> data = data;
+    new_stack_eliment -> next = NULL;
+
+    return new_stack_eliment;
+}
+
+void print_stack(struct node* top, int *size_stack){
+
+    printf("   _         _   Size of stack - %d\n", *size_stack);
+    printf("    |       |   \n");
+    printf("    |       |   \n");
+    while(top != NULL){
+
+        if(*size_stack != 0){
+            if(top -> data > 9){
+                printf("    |-------|   \n");
+                printf("    |  %d   |   \n", top -> data);
+            }else if(top -> data > 99){
+                printf("    |-------|   \n");
+                printf("    |  %d  |   \n", top -> data);
+            }else if(top -> data > 999){
+                printf("    |-------|   \n");
+                printf("    | %d  |   \n", top -> data);
+            }else if(top -> data > 9999){
+                printf("    |-------|   \n");
+                printf("    | %d |   \n", top -> data);
+            }else if((top -> data <= 9) && (top -> data >= 0)){
+                printf("    |-------|   \n");
+                printf("    |   %d   |   \n", top -> data);
+            }else{
+
+            }
+        }
+        top = top -> next;
+    }
+
+    printf("     -------   \n");
+}
+
+struct node* free_memory(struct node* top, int n){
+    struct node* now;
+    if(n == 0){
+        while(top != NULL){
+            now = top;
+            top = now -> next;
+
+            free(now);
+        }
+    }else{
+        for(int i = 0; i < n; i++){
+            now = top;
+            top = now -> next;
+
+            free(now);
+        }
+
+        return top;
+    }
+}
+
+//stack implimentation
+int main(){
+    int size_stack = 0;
+    struct node* top;
+    struct node* now = NULL;
+    int input = -1;
+
+repit:
+
+    printf("Clearing the terminal...\n");
+    system("cls");
+    
+    print_stack(top, &size_stack);
+
+    printf("\n\n-----------------------------------------------------------\n\n");
+
+    printf("0. Exit\n");
+    printf("1. Push 10 eliment to stack '1 to 10'\n");
+    printf("2. Push 'n' eliment to stack\n");
+    printf("3. Pop '1' Eliment from top\n");
+    printf("4. Pop 'n' Eliment from top\n\n==> ");
+
+    scanf("%d", &input);
+
+    if(input == 1){
+        for(int i = 0; i < 10; i++){
+            top = new_stack_eliment(i + 1);
+            top -> next = now;
+
+            now = top;
+            size_stack++;
+        }
+    }
+
+    if(input == 2){
+        int eliment_n, eliment = 0;
+        printf("How many eliment you want to Push\n ==> ");
+        scanf("%d", &eliment);
+
+        for(int i = 0; i < eliment; i++){
+            printf("  ==> ");
+            scanf("%d", &eliment_n);
+
+            top = new_stack_eliment(eliment_n);
+            top -> next = now;
+            
+            now = top;
+            size_stack++;
+        }
+    }
+
+    if(input == 3){
+        top = free_memory(top, 1);
+        now = top;
+
+        size_stack--;
+    }
+
+    if(input == 4){
+        int eliment;
+        printf("How many eliment you want to Pop\n ==> ");
+        scanf("%d", &eliment);
+
+        top = free_memory(top, eliment);
+        now = top;
+
+        size_stack-=eliment;
+    }
+
+    if(input == 0){
+        // free memory and end the code
+        free_memory(top, 0);
+
+        goto end;
+    }
+
+    goto repit;
+
+end:
+    return 0;
+}
+
+
+
